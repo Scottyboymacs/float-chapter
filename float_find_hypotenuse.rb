163738b922a083af1484ca3b3e9fb3ba1d1fd436
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-04-14 15:46:40 +0000

    Final build "float"

--- float_find_hypotenuse.rb
+++ float_find_hypotenuse.rb
@@ -5,3 +5,11 @@
 # 5.3
 
 
+short_1 = 2.8
+short_2 = 4.5
+
+a2_b2 = (short_1*short_1) + (short_2*short_2)
+
+hyp = Math.sqrt(a2_b2)
+
+p hyp
--- float_round.rb
+++ float_round.rb
@@ -1,2 +1,11 @@
 # Output ten divided by 3 rounded to 3 decimal places.
   
+float = 10/3.0 
+
+p float.round(3)
+
+#float = float.round(5)
+
+#p float
+
+#p float.round(7)
